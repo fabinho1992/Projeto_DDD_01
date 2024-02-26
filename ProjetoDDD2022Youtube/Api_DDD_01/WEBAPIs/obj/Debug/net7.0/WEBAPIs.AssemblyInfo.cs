5a8840807700fb35ad7d9f6086a3351c2c40ec5f
fabinho1992 <f.santosdev1992@gmail.com>
2024-02-26 20:03:20 -0300

    Mensagem do commit

--- ProjetoDDD2022Youtube/Api_DDD_01/WEBAPIs/obj/Debug/net7.0/WEBAPIs.AssemblyInfo.cs
+++ ProjetoDDD2022Youtube/Api_DDD_01/WEBAPIs/obj/Debug/net7.0/WEBAPIs.AssemblyInfo.cs
@@ -12,12 +12,12 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
-[assembly: System.Reflection.AssemblyCompanyAttribute("WEBAPIs")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WebAPIs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c00a476cb00e27247b29bec4dfd0802c2e3948e")]
-[assembly: System.Reflection.AssemblyProductAttribute("WEBAPIs")]
-[assembly: System.Reflection.AssemblyTitleAttribute("WEBAPIs")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6556be6e915bf76ed40f54d2773fc9bf84788307")]
+[assembly: System.Reflection.AssemblyProductAttribute("WebAPIs")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WebAPIs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
